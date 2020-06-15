711e5498657127adc3b31f2db65aa58babd08ff9
Saurav Chaudhary <59189555+sauravchaudharysc@users.noreply.github.com>
2020-06-15 09:38:00 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+//jshint esversion : 6
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app= express();
+
+//To render Static File
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options ={
+    url: "https://us18.api.mailchimp.com/3.0/lists/ Get it at mailchimp",
+    method: "POST",
+     headers: {
+       "Authorization": "Get Your own at mailchimp"
+     },
+     body: jsonData
+  };
+  request(options,function(error, response, body){
+    if(error){
+      res.sendFile(__dirname + "/failure.html");
+    }else{
+      if(response.statusCode === 200){
+        res.sendFile(__dirname + "/sucess.html");
+      }else{
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+});
+
+//Use 3000 for local server
+//For heroku dynamic port is required
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server is running in port 3000");
+});
+
